e96c0240b3764c9c172edaaa2954dc83517fa6a4
Fernanda Rodriguez <rodriguezfer2811@gmail.com>
2023-08-22 13:39:42 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ETAPA 1 - Entrega miércoles 23/08
+
+```sh
+TRABAJO DE HTML Y CSS
+```
+
+## Nomenclatura
+```sh
+64986_digitalers-proyecto-integrador-etapa-uno_Rodriguez-Fernanda.zip 
+```
+
+## Debe contener: 
+```sh
+>> Archivo txt (Rodríguez_Fernanda.txt)
+    ** Nombre y Apellido. 
+    ** URL Repostorio de Github.
+    ** URL Proyecto subido a Netlify
+    ** URL Figma-Escalidraw
+```
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { resolve } from 'node:path'
+
+console.log(resolve('pages'))
+
+export default {
+    server: {
+        port: "2222",
+    },
+    css: {
+        devSourcemap: true,
+    },
+    build: {
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                carrito: resolve('pages/carrito.html'),
+                index: resolve('pages/index.html'),
+                contacto: resolve('pages/contacto.html')
+            }
+        }
+    }
+}
